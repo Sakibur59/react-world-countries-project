7ce1bab90e4800e0cb48ebe0c8607d28ce611ac8
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-14 22:14:23 +0600

    displaying countries img,population,region

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Country = ({country}) => {
     return (
         <div>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            <p>Region: {country.region.region}</p>
         </div>
     );
 };
